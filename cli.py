07c323ea670e95b5f3111eaad322a335c6bdcbfa
0ceanSlim <89587889+0ceanSlim@users.noreply.github.com>
2023-11-09 12:10:46 -0500

    cli improvements, help defined in src

--- cli.py
+++ cli.py
@@ -4,12 +4,14 @@ import subprocess
 import sys
 from src.help import help
 
+
 def clear_console():
     # Clear console based on the operating system
-    if os.name == 'nt':  # For Windows
-        os.system('cls')
+    if os.name == "nt":  # For Windows
+        os.system("cls")
     else:  # For Unix/Linux/Mac
-        os.system('clear')
+        os.system("clear")
+
 
 def check_python_installation():
     try:
@@ -21,16 +23,19 @@ def check_python_installation():
     except FileNotFoundError:
         return False
 
+
 def install_python():
     print("Python is not installed. Please download and install it from:")
     print("https://www.python.org/downloads/")
     input("Press Enter to continue after installing Python...")
     sys.exit(1)
 
+
 def check_rpc_config():
     if os.path.exists("rpc_config.json"):
         return True
     return False
+
 
 def get_rpc_config():
     rpc_host = input("Enter the RPC host: ")
@@ -48,14 +53,16 @@ def get_rpc_config():
     with open("rpc_config.json", "w") as config_file:
         json.dump(rpc_config, config_file)
 
+
 def run_script(script_name):
     script_path = os.path.join("src", f"{script_name}.py")
 
     if os.path.exists(script_path):
-        print(f"Running {script_name}...")
+        # print(f"Running {script_name}...")
         os.system(f"python {script_path}")
     else:
         print(f"Error: {script_name} not found!")
+
 
 def main():
     setup_completed = False
@@ -93,14 +100,15 @@ def main():
     while True:
         user_input = input("Enter a command: ")
 
-        if user_input.lower() == 'exit':
+        if user_input.lower() == "exit":
             break
-        if user_input.lower() == 'help':
+        if user_input.lower() == "help":
             help()
         if setup_completed:
             run_script(user_input)
         else:
             print("Error: Setup not completed. Use 'help' to see available commands.")
+
 
 if __name__ == "__main__":
     main()
--- src/help.py
+++ src/help.py
@@ -1,20 +1,18 @@
-print("\nAvailable Commands:")
-print("createWallet - Create a Wallet on your Elements Node")
-print("getNewAddress - Prompts for wallet selection, then generates a new receiving address")
-print("listTransactions - Prompts for wallet selection, then lists all the transactions made in the selected wallet")
-print("listUnspent - Prompts for wallet selection, then lists the selected wallet's asset balances")
-print("listWallets - Lists all the Wallets that exist on your Elements Node")
-print("sendToAddress - Create and sign a new transaction to send assets from the selected Wallet")
-print("help - Display available commands")
-print("exit - Quit the program\n")
-
 def help():
     print("\nAvailable Commands:")
     print("createWallet - Create a Wallet on your Elements Node")
-    print("getNewAddress - Prompts for wallet selection, then generates a new receiving address")
-    print("listTransactions - Prompts for wallet selection, then lists all the transactions made in the selected wallet")
-    print("listUnspent - Prompts for wallet selection, then lists the selected wallet's asset balances")
+    print(
+        "getNewAddress - Prompts for wallet selection, then generates a new receiving address"
+    )
+    print(
+        "listTransactions - Prompts for wallet selection, then lists all the transactions made in the selected wallet"
+    )
+    print(
+        "listUnspent - Prompts for wallet selection, then lists the selected wallet's asset balances"
+    )
     print("listWallets - Lists all the Wallets that exist on your Elements Node")
-    print("sendToAddress - Create and sign a new transaction to send assets from the selected Wallet")
+    print(
+        "sendToAddress - Create and sign a new transaction to send assets from the selected Wallet"
+    )
     print("help - Display available commands")
     print("exit - Quit the program\n")
