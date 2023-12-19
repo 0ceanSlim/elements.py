b6b061b94542c2bb1f862d9e62c252c57b5a8a19
0ceanSlim <chriskerr@gmx.com>
2023-12-19 16:59:39 -0500

    building flask app

--- app/app.py
+++ app/app.py
@@ -1,7 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+from ..src import create_wallet, generate_seed
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "hello world"
+@app.route('/')
+def show_wallet_form():
+    return render_template('index.html')
+
+@app.route('/create_wallet', methods=['POST'])
+def handle_create_wallet():
+    wallet_name = request.form['walletName']
+    result = create_wallet(wallet_name)
+    return jsonify({'message': result})
+
+@app.route('/generate_seed', methods=['POST'])
+def handle_generate_seed():
+    seed_length = request.form['seedLength']
+    result = generate_seed(seed_length)
+    return jsonify({'seed': result})
+
+if __name__ == "__main__":
+    app.run(debug=True)
